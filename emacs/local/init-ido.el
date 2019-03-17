ad69525d5bae884ccf3190c4b79f4372ad8ffea5
Tomas Bjurman <tbjurman@tail-f.com>
2019-03-17 17:11:21 +0100

    emacs: ido tweaks

--- emacs/local/init-ido.el
+++ emacs/local/init-ido.el
@@ -3,5 +3,7 @@
   :config
   (setq ido-enable-flex-matching t)
   (setq ido-everywhere t)
+  (setq confirm-nonexistent-file-or-buffer nil)
+  (setq ido-create-new-buffer 'always)
   (ido-mode 1))
 (provide 'init-ido)
