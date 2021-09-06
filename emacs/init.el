78e2c134da91c39b7f73eb916b0ba4cb3e912e58
Tomas Bjurman <tbjurman@cisco.com>
2021-09-06 16:37:19 +0200

    emacs: bup

--- emacs/init.el
+++ emacs/init.el
@@ -268,3 +268,10 @@
   :ensure t
   :config
   (add-hook 'after-init-hook 'global-company-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yang-mode yaml-mode sql-indent solarized-theme smex rustic realgud-lldb magit lux-mode lsp-ui ivy-hydra graphviz-dot-mode git-gutter flycheck erlang creamsody-theme company base16-theme ahungry-theme)))
