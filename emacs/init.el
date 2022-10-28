48404cb6d17c454b37e1160c350fea3575329c29
Tomas Bjurman <tbjurman@cisco.com>
2022-10-28 14:52:08 +0200

    emacs: remove rustic/lsp

--- emacs/init.el
+++ emacs/init.el
@@ -258,64 +258,7 @@ apps are not started from a shell."
 ;; --- Vim style stuff (end) -----------------------------------------
 
 ;; ############################################################################
-(use-package rust-mode
-  :ensure t)
-
-;; ############################################################################
-;; (use-package rustic
-;;   :ensure t
-;;   :bind (:map rustic-mode-map
-;;               ("C-c C-c j" . lsp-ui-imenu)
-;;               ("C-c C-c ?" . lsp-find-references)
-;;               ("C-c C-c l" . flycheck-list-errors)
-;;               ("C-c C-c a" . lsp-execute-code-action)
-;;               ("C-c C-c r" . lsp-rename)
-;;               ("C-c C-c q" . lsp-workspace-restart)
-;;               ("C-c C-c Q" . lsp-workspace-shutdown)
-;;               ("C-c C-c s" . lsp-rust-analyzer-status))
-;;   :config
-;;   ;; uncomment for less flashiness
-;;   ;; (setq lsp-eldoc-hook nil)
-;;   ;; (setq lsp-enable-symbol-highlighting nil)
-;;   ;; (setq lsp-signature-auto-activate nil)
-
-;;   ;; comment to disable rustfmt on save
-;;   (setq rustic-format-on-save t)
-;;   (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
-
-;; (defun rk/r>84;0;0custic-mode-hook ()
-;;   ;; so that run C-c C-c C-r works without having to confirm
-;;   (setq-local buffer-save-without-query t))
-
-;; ############################################################################
-;; (use-package flycheck :ensure t)
-;; ############################################################################
-;; (use-package lsp-mode
-;;   :ensure
-;;   :commands lsp
-;;   :custom
-;;   ;; what to use when checking on-save. "check" is default, I prefer clippy
-;;   (lsp-rust-analyzer-cargo-watch-command "clippy")
-;;   (lsp-eldoc-render-all t)
-;;   (lsp-idle-delay 0.5)
-;;   (lsp-rust-analyzer-server-display-inlay-hints nil))
-
-  ;; :hook ((rustic-mode . lsp)
-  ;;        (lsp-mode . lsp-enable-which-key-integration)))
-  ;; :config
-  ;; (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-;; ############################################################################
-;; (use-package lsp-ui
-;;   :ensure
-;;   :commands lsp-ui-mode
-;;   :custom
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-sideline-show-hover nil)
-;;   (lsp-ui-doc-enable t))
-
-;; ############################################################################
-;; (use-package rust-mode :ensure t)
+(use-package rust-mode :ensure t)
 
 ;; ############################################################################
 (use-package sql-indent :ensure t)
